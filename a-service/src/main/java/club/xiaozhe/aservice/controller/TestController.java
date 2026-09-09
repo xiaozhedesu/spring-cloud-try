8cfaa8dea8d4bc935cf79c71a34206296c2e3819
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-09-09 13:53:25 +0800

    update: 去掉请求前缀，由Gateway进行分发

--- a-service/src/main/java/club/xiaozhe/aservice/controller/TestController.java
+++ a-service/src/main/java/club/xiaozhe/aservice/controller/TestController.java
@@ -1,11 +1,9 @@
 package club.xiaozhe.aservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/a")
 public class TestController {
     @GetMapping("/test")
     public String test() {
--- b-service/src/main/java/club/xiaozhe/bservice/controller/TestController.java
+++ b-service/src/main/java/club/xiaozhe/bservice/controller/TestController.java
@@ -1,11 +1,9 @@
 package club.xiaozhe.bservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/b")
 public class TestController {
     @GetMapping("/test")
     public String test() {

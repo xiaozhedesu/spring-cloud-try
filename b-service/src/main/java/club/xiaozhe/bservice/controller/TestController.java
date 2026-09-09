c94f4d7d98abc71e01b7876f4148b2dc6f7a2287
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-09-09 15:09:05 +0800

    add: 添加/port接口，观察b-service的端口

--- b-service/src/main/java/club/xiaozhe/bservice/controller/TestController.java
+++ b-service/src/main/java/club/xiaozhe/bservice/controller/TestController.java
@@ -1,12 +1,21 @@
 package club.xiaozhe.bservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/test")
     public String test() {
         return "Ciallo! by B-service";
+    }
+
+    @GetMapping("/port")
+    public int port() {
+        return port;
     }
 }

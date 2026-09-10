fa41083321b14b0620fb48e6ec109862c001ebc9
xiaozhedesu <xiaozhedesu0219@outlook.com>
2026-09-10 20:37:20 +0800

    add: 编写common-core模块，新增测试工具类TestUtils

---
+++ common-core/src/main/java/club/xiaozhe/utils/TestUtils.java
@@ -0,0 +1,12 @@
+package club.xiaozhe.utils;
+
+/**
+ * 测试工具类，只用于测试其他微服务能否使用该模块的内容
+ */
+public final class TestUtils {
+    private TestUtils() {}
+
+    public static String test() {
+        return "U got it!";
+    }
+}
